981055f66262a683833b2b2ce7a2465e858cfaf4
themysterysolver <arjunprabhakar1910@gmail.com>
2025-02-24 21:14:47 +0530

    FEAT:tile+main

---
+++ clock.js
@@ -0,0 +1,11 @@
+class Tile{
+    constructor(time,inc){
+        this.time=time;
+        this.increment=inc;
+        this.element=document.createElement("div");
+    }
+}
+function main(){
+    
+}
+main();
