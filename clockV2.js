4a58514f6c77ea0316367596b5ed30517e25ec5a
themysterysolver <arjunprabhakar1910@gmail.com>
2025-02-25 07:21:07 +0530

    FIX+FEAT:URLgetParam works+Middle buttons

--- clockV2.js
+++ clockV2.js
@@ -1,16 +1,20 @@
-function mainV2(){
-    const URL=new URLSearchParams(window.location.search);
-    const type=URL.get("type");
-    const time=URL.get("time");
-    const inc=URL.get("increment"); 
-
-    console.log("Extracted type:", type);
-    console.log("Extracted time:", time);
-    console.log("Extracted increment:", inc);
-
+function populateMode(type,time,inc){
     let fortype=document.getElementById("fortype");
     let mode=document.createElement("div");
     mode.innerHTML=`${type}`;
     fortype.appendChild(mode);
+}
+function setMiddleButton(){
+    
+}
+function mainV2(){
+    
+    const URL=new URLSearchParams(window.location.search);
+    const type=URL.get("type");
+    const time=URL.get("time");
+    const inc=URL.get("increment"); 
+    
+    populateMode(type,time,inc);
+    setMiddleButton();
 }
 window.onload = mainV2;
