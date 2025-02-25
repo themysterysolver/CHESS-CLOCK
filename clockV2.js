8b8e9c1fecca24ab8a8f627b5d2636a1b58a4d67
themysterysolver <arjunprabhakar1910@gmail.com>
2025-02-25 14:15:52 +0530

    FEAT:added a timer

--- clockV2.js
+++ clockV2.js
@@ -5,7 +5,46 @@ function populateMode(type,time,inc){
     fortype.appendChild(mode);
 }
 function setMiddleButton(){
-    
+    return;
+}
+class chessClock{
+    constructor(time_white,increment_white,time_black,increment_black){
+        console.log("CP-1");
+
+        this.time_white=time_white;
+        this.time_black=time_black;
+        this.increment_white=increment_white;
+        this.increment_black=increment_black;
+
+        this.white_sec=this.minute_to_sec(this.time_white);
+        this.black_sec=this.minute_to_sec(this.time_black);
+        
+        this.setDisplay();
+    }
+    minute_to_sec(minute){
+        return minute*60;
+    }
+    sec_to_display(sec){
+        let mm=Math.floor(sec/60);
+        let ss=sec%60;
+        return `${mm<10 ? "0":""}${mm}:${ss<10 ? "0":""}${ss}`;
+    }
+    setDisplay(){
+        let l=document.createElement("div");
+        let r=document.createElement("div");
+        l.id="l";
+        r.id="r";
+        let left=document.getElementById("left");
+        let right=document.getElementById("right");
+        left.appendChild(l);
+        right.appendChild(r);
+        this.display();
+    }
+
+    display(){
+        document.getElementById("l").innerHTML=`${this.sec_to_display(this.white_sec)}`;
+        document.getElementById("r").innerHTML=`${this.sec_to_display(this.black_sec)}`;
+    }
 }
 function mainV2(){
     
@@ -16,5 +55,8 @@ function mainV2(){
     
     populateMode(type,time,inc);
     setMiddleButton();
+    if(type!="custom"){
+        const clock=new chessClock(time,inc,time,inc);
+    }
 }
 window.onload = mainV2;
